8862fef2da924a6d04ddd6fc912e3a16014c195c
julianjimenezm <79744990+julianjimenezm@users.noreply.github.com>
2021-02-27 10:55:16 +0100

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 30 21:23:07 2021
 
-@author: jimenez
-"""
 
-# spotify keys information
-token ="BQCclpT4470NRDloq35qw-f8V-r7m2FGk6iQGalAscL62LpyiZmAP8gniwYiiI9Bafp5UhERNBrJnOIxapXqmxUpsmh0A9uBiIKKUDzAtB8ACHB-Fi9kdhJc6As5uJ2kQ75iKQQXZyzbyxmOQyoSZPnNq56fFAfg0-dlTxO_eaV4kC0azMmEPrmxjj1hu5OFasXh27kyEvc-CE8DobHvLI4"
-user_id = "davidmej49"
+# Spotify Keys information
+
+spotify_token ="your_request_token_expires_every_hour"
+spotify_user_id = "your_user_id"
                 
